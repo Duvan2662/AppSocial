3057033ffe9bd2206924917693dea8a7acd53e39
Duvan <duvanriano2626@gmail.com>
2023-04-30 09:37:08 -0500

    layout de carga y autenticacio de usuarios en realtime firebase

--- app/src/main/java/com/example/parche_ud/auth/LoginActivity.kt
+++ app/src/main/java/com/example/parche_ud/auth/LoginActivity.kt
@@ -6,13 +6,19 @@ import android.os.Bundle
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.parche_ud.MainActivity
+import com.example.parche_ud.R
 import com.example.parche_ud.databinding.ActivityLoginBinding
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import java.util.concurrent.TimeUnit
 
 class LoginActivity : AppCompatActivity() {
@@ -21,12 +27,18 @@ class LoginActivity : AppCompatActivity() {
     //Variable que me ayuda a autenticar el usuario
     val auth = FirebaseAuth.getInstance()
 
+    //Me maneja el id del usuario
     private var verificarID : String? = null
+
+    //dialogo e alerta
+    private  lateinit var dialogo : AlertDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Crea una alerta de dialogo con su constructor que carga el layout de carga no se puede cancelar
+        dialogo = AlertDialog.Builder(this).setView(R.layout.cargar_layout).setCancelable(false).create()
 
 
         //Me ayuda a enviar el codigo a la funcion que verifica el numero
@@ -53,6 +65,8 @@ class LoginActivity : AppCompatActivity() {
     // por el usuario y autenticarlo en la aplicación de Android utilizando Firebase Authentication.
     private fun verificarCodigo(codigo: String) {
 //        binding.sendOtp.showLoadingButton()
+
+        dialogo.show()
         val credential = PhoneAuthProvider.getCredential(verificarID!!, codigo)//Crea un objeto PhoneAuthCredential
         signInWithPhoneAuthCredential(credential)
     }
@@ -61,6 +75,7 @@ class LoginActivity : AppCompatActivity() {
 
 //        binding.sendOtp.showLoadingButton() //metodo del boton de github
 
+        dialogo.show()
         //Envía un código de verificación al teléfono del usuario
         val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
@@ -68,6 +83,8 @@ class LoginActivity : AppCompatActivity() {
             //al usuario con las credenciales de verificación de teléfono proporcionadas.
             override fun onVerificationCompleted(credential: PhoneAuthCredential) {
 //                binding.sendOtp.showNormalButton()
+
+                //dialogo.dismiss()
                 signInWithPhoneAuthCredential(credential)
             }
 
@@ -84,6 +101,7 @@ class LoginActivity : AppCompatActivity() {
             ) {
                 this@LoginActivity.verificarID = verificarID
 
+                dialogo.dismiss()
 //                binding.sendOtp.showNormalButton()
                 binding.numeroLayout.visibility = GONE
                 binding.codigoLayout.visibility = VISIBLE
@@ -106,12 +124,42 @@ class LoginActivity : AppCompatActivity() {
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->//se verifica si la operación de inicio de sesión fue exitosa
 //                binding.sendOtp.showNormalButton()
+
                 if (task.isSuccessful) {
-                    startActivity(Intent(this,MainActivity::class.java))//Si es correcta la autenticacion entra al main activity
-                    finish()//se utiliza para finalizar la actividad actual y eliminarla
+
+                    verificarUsuarioExiste(binding.numeroUsuario.text.toString())
+
                 } else {
+
+                    dialogo.dismiss()
                     Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()//Mensaje de error si no es correcta la autenticacion
                 }
             }
+    }
+
+    //Me ayuda a ver si es correcta la autenticacion
+    private fun verificarUsuarioExiste(numero: String) {
+
+        FirebaseDatabase.getInstance().getReference("Usuarios").child(numero)//Revisa en la tabla usuarios si existe el numero que el usuario ingreso
+            .addValueEventListener(object : ValueEventListener{
+
+                //Error en la base d datos
+                override fun onCancelled(error: DatabaseError) {
+                    dialogo.dismiss()
+                    Toast.makeText(this@LoginActivity,error.message,Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.exists()){
+                        dialogo.dismiss()
+                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))//Si existe en la base de datos ingresa a la aplicacion
+                        finish()
+                    }
+                    else{
+                        startActivity(Intent(this@LoginActivity,RegistroActivity::class.java))//Si no existe en la base de datos ingresa al Registro de la aplicacion
+                        finish()
+                    }
+                }
+            })
     }
 }
