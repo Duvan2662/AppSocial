97f8565aa75c2c4b2455d6c8eaf55d14b6718523
Duvan <duvanriano2626@gmail.com>
2023-05-04 19:33:52 -0500

    Creacion de la barra de navegacion con Android Jetpack, ademas se  inicia la creacion de los fragmentos de las vista de mensajes, perfil y amigos

--- app/src/main/java/com/example/parche_ud/MainActivity.kt
+++ app/src/main/java/com/example/parche_ud/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.parche_ud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.parche_ud.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //esta línea de código establece la configuración de la navegación de la aplicación
+        // utilizando el componente de navegación de Android Jetpack.
+        val controladorNavegacion = findNavController(R.id.fragmento)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,controladorNavegacion)
     }
 }
