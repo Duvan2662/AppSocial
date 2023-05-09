b8dcbdb41ac4e56667b3f390ca2954c9dc51ade2
Duvan <duvanriano2626@gmail.com>
2023-05-08 20:12:42 -0500

    Se colocan las dependecias de la libreria que me va a meanejarel movimiento de izquierda a derecha para hacer duo o rechazar

--- app/src/main/java/com/example/parche_ud/utilidades/AmigosFragment.kt
+++ app/src/main/java/com/example/parche_ud/utilidades/AmigosFragment.kt
@@ -1,60 +1,46 @@
 package com.example.parche_ud.utilidades
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.parche_ud.R
+import com.example.parche_ud.databinding.FragmentAmigosBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AmigosFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AmigosFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding : FragmentAmigosBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_amigos, container, false)
+        binding = FragmentAmigosBinding.inflate(layoutInflater)
+
+        getData()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AmigosFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AmigosFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun getData() {
+        FirebaseDatabase.getInstance().getReference("usuarios").addValueEventListener(object :
+            ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.d("shub","onDataChange: ${snapshot.toString()}")
             }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
+
+
 }
