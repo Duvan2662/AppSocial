1860035e52fde4b1c534157e44b42498d1d1b758
Duvan <duvanriano2626@gmail.com>
2023-05-23 21:41:50 -0500

    Ya se implementa la  funcion de mostrar los usuarios con cierta informacion en las cardViews con imagen de cada uno

---
+++ app/src/main/java/com/example/parche_ud/adapter/AmistadAdaptador.kt
@@ -0,0 +1,30 @@
+package com.example.parche_ud.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.parche_ud.databinding.UsuariosLayoutBinding
+import com.example.parche_ud.model.usuariosModelo
+
+
+//Adaptador que me sirve para ir colocando las imagens y la informacion de los usuarios en el usuarioLayout
+class AmistadAdaptador(val context : Context, val lista: ArrayList<usuariosModelo>) :RecyclerView.Adapter<AmistadAdaptador.verAmistades>() {
+    inner class verAmistades(val binding:UsuariosLayoutBinding):RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): verAmistades {
+        return verAmistades(UsuariosLayoutBinding.inflate(LayoutInflater.from(context),parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return lista.size
+    }
+
+    override fun onBindViewHolder(holder: verAmistades, position: Int) {
+        holder.binding.textView5.text = lista[position].nombre //Nombre del usuario en la carta
+        holder.binding.textView4.text = lista[position].correo //Correo del usuario en la carta
+
+        Glide.with(context).load(lista[position].imagen).into(holder.binding.usuarioImagen)//Imagen del usuario en la carta
+    }
+}
--- app/src/main/java/com/example/parche_ud/utilidades/AmigosFragment.kt
+++ app/src/main/java/com/example/parche_ud/utilidades/AmigosFragment.kt
@@ -7,39 +7,106 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.example.parche_ud.R
+import androidx.recyclerview.widget.DefaultItemAnimator
+import com.example.parche_ud.adapter.AmistadAdaptador
 import com.example.parche_ud.databinding.FragmentAmigosBinding
+import com.example.parche_ud.model.usuariosModelo
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.ktx.Firebase
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.Direction
 
 
 class AmigosFragment : Fragment() {
 
     private lateinit var binding : FragmentAmigosBinding
+    private lateinit var pendiente : CardStackLayoutManager
+    private lateinit var lista:ArrayList<usuariosModelo>//Donde se van a guardar todos los usuarios de firebase
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAmigosBinding.inflate(layoutInflater)
 
-        getData()
+        obtenerDatos()
         return binding.root
     }
 
-    private fun getData() {
+
+
+    //Obtiene los datos de firebase y los pone en una lista para mostrarlos en el
+    // reciclerView del adapatdor donde se van a mostar  los datos de los usuarios (Imagen, correo, nombre)
+    private fun obtenerDatos() {
         FirebaseDatabase.getInstance().getReference("usuarios").addValueEventListener(object :
             ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 Log.d("shub","onDataChange: ${snapshot.toString()}")
+                if(snapshot.exists()){
+                     lista = arrayListOf<usuariosModelo>()
+                    for(datos in snapshot.children){
+                        val modelo = datos.getValue(usuariosModelo::class.java)
+                        lista.add(modelo!!)
+                    }
+                    lista.shuffle()//Mezcla aleatoriamente los usuarios
+                    iniciar()
+                    binding.cartavistas.layoutManager = pendiente
+                    binding.cartavistas.itemAnimator = DefaultItemAnimator()//gestionar las animaciones
+                    binding.cartavistas.adapter = AmistadAdaptador(requireContext(),lista)
+                }else{
+                    Toast.makeText(requireContext(), "Hubo un error", Toast.LENGTH_SHORT).show()
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show()
             }
         })
+    }
+    //Esta funcion es practicamentre la que le de la la animacion a cada carta
+    private fun iniciar() {
+        pendiente = CardStackLayoutManager(requireContext(), object : CardStackListener{
+            override fun onCardDragging(direction: Direction?, ratio: Float) {
+
+            }
+
+            //Se invoca cuando se realiza un deslizamiento completo de una tarjeta en una dirección específica
+            override fun onCardSwiped(direction: Direction?) {
+                //verifica si la tarjeta superior en la pila es la última y muestra un mensaje de "última carta
+                if(pendiente.topPosition == lista.size){
+                    Toast.makeText(requireContext(), "Esta es la ultima carta", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCardRewound() {
+
+            }
+
+            override fun onCardCanceled() {
+
+            }
+
+            override fun onCardAppeared(view: View?, position: Int) {
+
+            }
+
+            override fun onCardDisappeared(view: View?, position: Int) {
+
+            }
+
+        })
+        //Establece el número de tarjetas visibles simultáneamente en la pila en 3
+        pendiente.setVisibleCount(3)
+        //Define el valor de la distancia de desplazamiento entre cada tarjeta adyacente en la pila.
+        pendiente.setTranslationInterval(0.6f)
+        //Define el valor de la escala de tamaño entre cada tarjeta adyacente en la pila.
+        pendiente.setScaleInterval(0.8f)
+        //Establece el ángulo máximo de rotación en grados para las tarjetas en la pila.
+        pendiente.setMaxDegree(20.0f)
+        // Establece la dirección en la que se pueden desplazar las tarjetas, en este caso, horizontalmente.
+        pendiente.setDirections(Direction.HORIZONTAL)
     }
 
 
